c647b4efcca9935c31b7249322fc152d8bff2bdd
oweson <570347720@qq.com>
2019-09-07 16:13:55 +0800

    复习第八天的函数；

---
+++ mook/regex/Demo1.py
@@ -0,0 +1,4 @@
+import re
+
+result = re.match("itcast", "itcast.cn")
+print(result.group())
--- 第六天函数/09-带有参数的函数.py
+++ 第六天函数/09-带有参数的函数.py
@@ -6,8 +6,19 @@ def sum_2_nums(a, b):
     print("%d+%d=%d" % (a, b, result))
 
 
+def simple_Add(a, b):
+    total = a + b
+    print("%d+%d=%d" % (a, b, total))
+
+
+def print_One(a):
+    print("%d is? :" % a)
+
+
 num1 = int(input("请输入第1个数字:"))
 num2 = int(input("请输入第2个数字:"))
 
 # 2 调用函数
 sum_2_nums(num1, num2)
+simple_Add(1, 2)
+print_One(100)
